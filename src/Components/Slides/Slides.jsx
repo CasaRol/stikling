0650214cefe31834f5bad6b1bb2db8f66d9ee425
CasaRol <alexander-rol@hotmail.com>
2022-02-23 19:55:19 +0100

    Added first few pictures of Stikling

--- src/Components/Slides/Slides.jsx
+++ src/Components/Slides/Slides.jsx
@@ -1,14 +1,14 @@
 //import { useEffect, useState } from 'react'
 import '../../Styling.css'
 
-
+let picture = "../images/stiklingScanning/Stikling_1.jpg"
 
 const Slides = (() => {
     //const [imgURL, setImgURL] = useState();
 
     return (
         <div className="centering">
-            <img style={{ margin: "2rem", border: "2px solid black" }} src='/examplePhoto.jpg' alt="Not found" />
+            <img style={{ margin: "2rem", border: "2px solid black" }} src={picture} alt="Not found" />
         </div>
     )
 })
