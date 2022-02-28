39fcbbc3e661000da94751ff6c948da52f73c5c0
CasaRol <alexander-rol@hotmail.com>
2022-02-28 16:50:35 +0100

    even more background sizing

--- src/Main.jsx
+++ src/Main.jsx
@@ -10,7 +10,7 @@ function Main() {
 
     return (
         <div className="girly pb-5">
-            <h1 className="d-flex justify-content-center align-items-center w-100 p-3"><strong>Baby girl Stikling nedtælling</strong></h1>
+            <h1 className="d-flex justify-content-center align-items-center w-100 pt-3 pb-3"><strong>Baby girl Stikling nedtælling</strong></h1>
             {/* Date is formatted as MM/dd/yyyy HH:mm:ss */}
             <div className="d-flex justify-content-center align-items-center w-100">
                 <img className="profilePicture border border-dark" src="./Julie.jpg" alt="Julie not found" />
@@ -19,7 +19,7 @@ function Main() {
                     <img className="profilePicture border border-dark" src="./Mathias.jpg" alt="Mathias not found" />
                 </div>
             </div>
-            <div className="row">
+            <div className="row pt-2">
                 <div className="col d-flex align-items-center">
                     <SizeComparison timestampMs={dateToMs(dueDate)} />
                 </div>
