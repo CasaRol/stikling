59acaeeb3e799aa0a9bcdd619155ae31fb718bf6
CasaRol <32219634+CasaRol@users.noreply.github.com>
2025-01-15 19:32:49 +0100

    Updated to latest react version

--- src/Components/Slides/Polaroid.jsx
+++ src/Components/Slides/Polaroid.jsx
@@ -1,12 +1,12 @@
 import stikling2 from '../../images/stiklingScanning/Stikling_2.jpg'
 import '../../Styling.css'
 
-const Polaroid = (({rotate}) => {
+const Polaroid = (({ rotate }) => {
 
     return (
         <div className="item" style={{ transform: `rotate(${rotate}deg)` }}>
             <div className="polaroid">
-                <img src={stikling2} />
+                <img src={stikling2} alt="resource not found" />
                 <div className="caption">INSERT CAPTION HERE</div>
             </div>
         </div>
