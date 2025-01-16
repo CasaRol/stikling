c08dca6cbbe860294e3bd288d01b1d651df243e3
Alexander <32219634+CasaRol@users.noreply.github.com>
2025-01-16 21:35:44 +0100

    Split code up into separate files to prepare for routing (#2)

--- src/Components/CountDownTimer/CountDown.jsx
+++ src/Components/CountDownTimer/CountDown.jsx
@@ -1,40 +1,82 @@
 import { useState, useEffect } from 'react'
 import './CountDownStyling.css'
-import { getRemainingTime } from '../../scripts'
+import { getRemainingTime, getPassedTime } from '../../scripts'
 
-const defaultRemainingTime = {
+const defaultTime = {
     seconds: '00',
     minutes: '00',
     hours: '00',
-    days: '00'
+    days: '00',
+    months: '00',
+    years: '00',
+    weeks: '00'
 }
 
-const CountDown = (({ timestampMs }) => {
+const CountDown = ((props) => {
+    var formattedDate;
 
-    const formattedDate = new Date(timestampMs).toLocaleString('da-DK', {
-        year: 'numeric',
-        month: '2-digit',
-        day: '2-digit',
-        hour12: false  // Use 24-hour format, or remove this for 12-hour format
-    });
+    if (!props.born) {
+        formattedDate = new Date(props.timestampMs).toLocaleString('da-DK', {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            hour12: false  // Use 24-hour format, or remove this for 12-hour format
+        });
+    } else {
+        formattedDate = new Date(props.timestampMs).toLocaleString('da-DK', {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit',
+            hour12: false  // Use 24-hour format, or remove this for 12-hour format
+        });
+    }
 
-    const [remainingTime, setRemainingTime] = useState(defaultRemainingTime);
+    const [timeCount, setTimeCount] = useState(defaultTime);
 
     useEffect(() => {
         const intervalId = setInterval(() => {
-            updateRemainingTime(timestampMs);
+            if (props.born) {
+                updatePassedTime(props.timestampMs)
+            } else {
+                updateRemainingTime(props.timestampMs);
+            }
         }, 1000)
         return () => clearInterval(intervalId);
-    }, [timestampMs])
+    }, [props.timestampMs])
 
-    function updateRemainingTime(countDown) {
-        setRemainingTime(getRemainingTime(countDown))
+    function updateRemainingTime(startingDateTime) {
+        setTimeCount(getRemainingTime(startingDateTime))
+    }
+
+    function updatePassedTime(startingDateTime) {
+        setTimeCount(getPassedTime(startingDateTime))
     }
 
     return (
         <div className="border border-dark border-3 rounded text-center w-50 p-4 bg-black bg-opacity-25">
-            <h2>{remainingTime.days} dage {remainingTime.hours} Timer {remainingTime.minutes} Minutter {remainingTime.seconds} Sekunder </h2>
-            <p><strong>{formattedDate}</strong></p>
+            {props.born ?
+                (<h2>
+                    {timeCount.years} {"år "}
+                    {timeCount.months} {timeCount.months > 1 ? "måneder " : "måned "}
+                    {timeCount.days} {timeCount.days > 1 ? "dage " : "dag "}
+                    {timeCount.hours} {timeCount.hours > 1 ? "timer " : "time "}
+                    {timeCount.minutes} {timeCount.minutes > 1 ? "minutter " : "minut "}
+                    {timeCount.seconds} {timeCount.seconds > 1 ? "sekunder " : "sekundt "}
+                </h2>) :
+                (<h2>
+                    {timeCount.days} {timeCount.days > 1 ? "dage " : "dag "}
+                    {timeCount.hours} {timeCount.hours > 1 ? "timer " : "time "}
+                    {timeCount.minutes} {timeCount.minutes > 1 ? "minutter " : "minut "}
+                    {timeCount.seconds} {timeCount.seconds > 1 ? "sekunder " : "sekundt "}
+                </h2>)
+            }
+            {!props.born ? <h3>({timeCount.weeks} {timeCount.weeks > 1 ? " uger " : " uge "} tilbage)</h3> : ("")}
+            <p>
+                {props.born ? "Født: " : "Anslået dato: "}
+                <strong>{formattedDate}</strong>
+            </p>
         </div >
     )
 })
--- src/scripts.js
+++ src/scripts.js
@@ -1,20 +1,20 @@
 import dayjs from 'dayjs'
+const utc = require('dayjs/plugin/utc');
+const timezone = require('dayjs/plugin/timezone');
 
-function dateToMs(date) {
-    return date.getTime()
-}
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 function getRemainingTime(timestampMs) {
-    const timestampGoal = dayjs(timestampMs);
+    const timestampGoal = dayjs(timestampMs).tz('Europe/Copenhagen'); //Accounts for daylight saving time
     const timestampNow = dayjs();
-
-
 
     return {
         seconds: getRemainingSeconds(timestampNow, timestampGoal),
         minutes: getRemainingMinutes(timestampNow, timestampGoal),
         hours: getRemainingHours(timestampNow, timestampGoal),
-        days: getRemainingDays(timestampNow, timestampGoal)
+        days: getRemainingDays(timestampNow, timestampGoal),
+        weeks: getRemainingWeeks(timestampNow, timestampGoal)
     }
 }
 
@@ -34,4 +34,50 @@ function getRemainingDays(timestampNow, timestampGoal) {
     return timestampGoal.diff(timestampNow, 'days');
 }
 
-export { getRemainingTime, dateToMs }
+function getRemainingWeeks(timestampNow, timestampGoal) {
+    return timestampGoal.diff(timestampNow, 'weeks');
+}
+
+//Passed time
+function getPassedTime(timestampMs) {
+    const timestampGoal = dayjs(timestampMs).tz('Europe/Copenhagen'); //Accounts for daylight saving time
+    const timestampNow = dayjs();
+
+    return {
+        seconds: getPassedSeconds(timestampNow, timestampGoal),
+        minutes: getPassedMinutes(timestampNow, timestampGoal),
+        hours: getPassedHours(timestampNow, timestampGoal),
+        days: getPassedDays(timestampNow, timestampGoal),
+        months: getPassedMonths(timestampNow, timestampGoal),
+        years: getPassedYears(timestampNow, timestampGoal)
+    }
+}
+
+function getPassedSeconds(timestampNow, timestampGoal) {
+    return timestampNow.diff(timestampGoal, 'seconds') % 60;
+}
+
+function getPassedMinutes(timestampNow, timestampGoal) {
+    return timestampNow.diff(timestampGoal, 'minutes') % 60;
+}
+
+function getPassedHours(timestampNow, timestampGoal) {
+    return timestampNow.diff(timestampGoal, 'hours') % 24;
+}
+
+function getPassedDays(timestampNow, timestampGoal) {
+    return Math.floor(timestampNow.diff(timestampGoal, 'days') % 30.43);
+}
+
+function getPassedMonths(timestampNow, timestampGoal) {
+    return timestampNow.diff(timestampGoal, 'months') % 12;
+}
+
+function getPassedYears(timestampNow, timestampGoal) {
+    return timestampNow.diff(timestampGoal, 'years');
+}
+
+
+
+
+export { getRemainingTime, getPassedTime }
